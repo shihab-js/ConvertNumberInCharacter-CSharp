4f9d058d32351c91a02cdf76efaee48f84094114
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-22 16:07:38 +0600

    Program has been completed....

--- ConvertNumberInCharacter/Program.cs
+++ ConvertNumberInCharacter/Program.cs
@@ -15,6 +15,60 @@ namespace ConvertNumberInCharacter
             //get input from user
             n = int.Parse(Console.ReadLine());
 
+            //create a while loop
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = (sum * 10) + r;
+                n = n / 10;
+            }
+            n = sum;
+
+            while (n > 0)
+            {
+                r = n % 10;
+
+                switch (r)
+                {
+                    case 1:
+                        Console.Write("One");
+                        break;
+                    case 2:
+                        Console.Write("Two");
+                        break;
+                    case 3:
+                        Console.Write("Three");
+                        break;
+                    case 4:
+                        Console.Write("Four");
+                        break;
+                    case 5:
+                        Console.Write("Five");
+                        break;
+                    case 6:
+                        Console.Write("Six");
+                        break;
+                    case 7:
+                        Console.Write("Seven");
+                        break;
+                    case 8:
+                        Console.Write("Eight");
+                        break;
+                    case 9:
+                        Console.Write("Nine");
+                        break;
+                    case 0:
+                        Console.Write("Zero");
+                        break;
+                    default:
+                        Console.Write("tttt");
+                        break;
+
+                }
+                n = n / 10;
+                Console.Write(" ");
+            }
+
         }
     }
 }
